391cfc8791ac13e44c526609db8bf7ed7851b60b
Jack Herrington <jherr@pobox.com>
2021-12-18 15:31:14 -0800

    More cleanups

--- getScheduleH.js
+++ getScheduleH.js
@@ -5,14 +5,17 @@ const yaml = require("js-yaml");
 const { getFields, extractFields } = require("./lib/extractFields");
 const getXMLData = require("./lib/getXMLData");
 
-if (process.argv.length < 3) {
-  console.log(`Usage: node getScheduleH.js <config.yml>`);
-  process.exit(0);
-}
-
 (async function () {
+  // Make sure we have a valid command line
+  if (process.argv.length < 3) {
+    console.log(`Usage: node getScheduleH.js <config.yml>`);
+    process.exit(0);
+  }
+
+  // Read the report configuration file
   const config = yaml.load(fs.readFileSync(process.argv[2], "utf8"));
 
+  // Get the header fields and trim them if the report configuration specifies fields
   let header = getFields();
   if (config.fields) {
     const regexes = config.fields.map((field) => new RegExp(field));
@@ -21,6 +24,7 @@ if (process.argv.length < 3) {
     );
   }
 
+  // Create the CSV report path
   const replacementValues = {
     minYear: Math.min(...config.years),
     maxYear: Math.max(...config.years),
@@ -31,20 +35,24 @@ if (process.argv.length < 3) {
     (_, p1) => replacementValues[p1.replace(/[{}]/g, "")]
   );
 
+  // Create the CSV writer
   const csvWriter = createCsvWriter({
     path,
     header,
   });
 
+  // Iterate through all the EINs
   const records = [];
   for (const ein of config.eins) {
     console.log(`Processing ${ein}`);
+
     const xmlReports = await getXMLData(ein, config.years);
+
     for (const year in xmlReports) {
       records.push(extractFields(xmlReports[year]));
     }
   }
 
-  // Write out the reports
+  // Write out the report
   await csvWriter.writeRecords(records);
 })();
