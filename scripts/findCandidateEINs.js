c910be684e1d920851c69fc53745b22dc956c21e
Jack Herrington <jherr@pobox.com>
2021-12-22 19:40:10 -0800

    Candidates update

--- scripts/findCandidateEINs.js
+++ scripts/findCandidateEINs.js
@@ -6,7 +6,16 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 const { DOMParser } = require("xmldom");
 
+const operatesHospitals = fs
+  .readFileSync("./operates-hospital-2018.txt")
+  .toString()
+  .split("\n")
+  .filter((t) => t.length);
+
 // Get the 2019, 2020, and 2021 indexes
+const einIndex2018 = JSON.parse(
+  fs.readFileSync("../indexes/index_2018.json").toString()
+).Filings2018;
 const einIndex2019 = JSON.parse(
   fs.readFileSync("../indexes/index_2019.json").toString()
 ).Filings2019;
@@ -18,10 +27,13 @@ const einIndex2021 = JSON.parse(
 ).Filings2021;
 
 // Create a lookup from EIN to name
-const nameLookup = [einIndex2021, einIndex2020, einIndex2019]
+const urlNameLookup = [einIndex2018, einIndex2021, einIndex2020, einIndex2019]
   .flat()
-  .reduce((lookup, { EIN, OrganizationName }) => {
-    lookup[EIN] = OrganizationName;
+  .reduce((lookup, { EIN, OrganizationName, URL }) => {
+    lookup[EIN] = {
+      URL,
+      OrganizationName,
+    };
     return lookup;
   }, {});
 
@@ -31,22 +43,21 @@ const einLookup = JSON.parse(fs.readFileSync("./einLookup.json").toString());
 (async () => {
   // Go through the target index and find all the 990s where
   // the organization name is interesting to us
-  const eins = einIndex2019
-    .filter(({ FormType }) => FormType === "990")
-    .filter(({ OrganizationName }) =>
-      OrganizationName.match(
-        /(hospital|health|medical|university|uny|college)/i
-      )
-    );
+  const eins = operatesHospitals.filter((ein) => !einLookup[ein]);
 
   // Go through the candidate EINs by index
   for (const index in eins) {
-    const nonProfit = eins[index];
+    const EIN = eins[index];
+    const nonProfit = urlNameLookup[EIN];
+
+    if (!nonProfit) {
+      console.log(`No non-profit found for ${EIN}`);
+    }
 
     // See if we have checked this already
-    if (einLookup[eins[index].EIN] === undefined) {
+    if (nonProfit && einLookup[EIN] === undefined) {
       console.log(
-        `${index} of ${eins.length}: ${nonProfit.EIN} : ${nonProfit.OrganizationName}`
+        `${index} of ${eins.length}: ${EIN} : ${nonProfit.OrganizationName}`
       );
 
       // Get the XML and convert it into a DOM
@@ -75,17 +86,17 @@ const einLookup = JSON.parse(fs.readFileSync("./einLookup.json").toString());
 
         // Format the data and add it to the lookup
         const data = {
-          EIN: nonProfit.EIN,
+          EIN,
           totalFunctionalExpenses,
           name,
         };
         console.log(JSON.stringify(data));
 
-        einLookup[nonProfit.EIN] = data;
+        einLookup[EIN] = data;
       } else {
         // Add a null to the lookup indicating that we checked the 990 but
         // didn't find a schedule H
-        einLookup[nonProfit.EIN] = null;
+        einLookup[EIN] = null;
       }
 
       // Update the lookup file so that if we crash we don't have to check again
@@ -107,6 +118,6 @@ const einLookup = JSON.parse(fs.readFileSync("./einLookup.json").toString());
   // Create the TSV file for the EINs that we checked but didn't find a schedule H
   const checked = Object.entries(einLookup)
     .filter(([_, data]) => data === null)
-    .map(([ein]) => [ein, nameLookup[ein]].join("\t"));
+    .map(([ein]) => [ein, urlNameLookup[ein].OrganizationName].join("\t"));
   fs.writeFileSync("checked.tsv", `EIN\tName\n${checked.join("\n")}`);
 })();
